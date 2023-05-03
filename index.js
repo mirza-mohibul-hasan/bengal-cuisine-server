b7dbcb2d9913a3f6490c150b741694ee6551b8df
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-05-03 10:19:03 +0600

    chefwise recipe data send added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json')
+const recipes = require('./data/recipes.json')
 
 app.get('/', (req, res) => {
     res.send('Recipe server is  running')
@@ -10,6 +11,16 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefs);
+});
+
+app.get('/recipes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const chefRecipe = recipes.filter(n => parseInt(n.chefId) === id);
+    res.send(chefRecipe)
+});
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
 });
 
 app.listen(port, () => {
